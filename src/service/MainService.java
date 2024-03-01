f4301910114094f7dadc2fd3b545a3d1319ed3f2
KarinaSkirmante <karina.krinkele@gmail.com>
2024-03-01 16:06:33 +0200

    Edit: mainservice with course testing

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.Course;
 import model.Degree;
 import model.Professor;
 import model.Student;
@@ -13,6 +14,8 @@ public class MainService {
 		System.out.println(pr2);
 		Professor pr3 = new Professor("Estere", "Vitola", Degree.mg);
 		System.out.println(pr3);
+		Professor pr4 = new Professor("Marcis", "Naktins", Degree.mg);
+		System.out.println(pr4);
 		
 		Student st1 = new Student();//Lara Bernardes student
 		System.out.println(st1);
@@ -20,6 +23,22 @@ public class MainService {
 		System.out.println(st2);
 		Student st3 = new Student("Davyd", "Akimov");
 		System.out.println(st3);
+		
+		Course c1 = new Course();
+		System.out.println(c1);
+		Course c2 = new Course("Data Structure", 2, pr3);
+		System.out.println(c2);
+		Course c3 = new Course("Networking", 4, pr4);
+		System.out.println(c3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 
 	}
